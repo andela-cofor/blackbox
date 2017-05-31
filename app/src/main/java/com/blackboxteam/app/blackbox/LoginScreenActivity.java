a18d4ebdecba45b6959305b05554061f9a044ddf
Chinedu Ofor <chineduofor@Chinedu-Ofor.local>
2017-05-31 15:29:05 +0100

    feature(loginScreen): add login screen to app[Start]

--- app/src/main/java/com/blackboxteam/app/blackbox/LoginScreenActivity.java
+++ app/src/main/java/com/blackboxteam/app/blackbox/LoginScreenActivity.java
@@ -1,13 +1,30 @@
 package com.blackboxteam.app.blackbox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginScreenActivity extends AppCompatActivity {
+public class LoginScreenActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        loginBtn = (Button) findViewById(R.id.button_login);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_login:
+                Intent intent = new Intent(LoginScreenActivity.this, TestPage.class);
+                startActivity(intent);
+        }
     }
 }
