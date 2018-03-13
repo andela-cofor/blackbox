77f31f94ff81780d4252f88e8fc44d922dbb668f
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2018-03-13 11:10:45 +0100

    remove unused imports

--- app/src/androidTest/java/com/blackboxteam/app/blackbox/LaunchScreenTest.java
+++ app/src/androidTest/java/com/blackboxteam/app/blackbox/LaunchScreenTest.java
@@ -6,20 +6,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.app.Instrumentation;
-import android.content.Intent;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.Assert.assertNotNull;
 
 /**
  * Test fo launch screen activity
