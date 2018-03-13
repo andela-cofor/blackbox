f85cd92bfd0d533fc42c7efd8b483432651ddd97
andela-cofor <oforchinedukelechi@gmail.com>
2018-03-13 17:01:41 +0100

    add user session persistence class

---
+++ app/src/main/java/com/blackboxteam/app/blackbox/util/UserSessionManager.java
@@ -0,0 +1,18 @@
+package com.blackboxteam.app.blackbox.util;
+
+/**
+ * Created by chineduofor on 13/03/2018.
+ */
+
+public class UserSessionManager {
+
+    private Boolean isActive = true;
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
