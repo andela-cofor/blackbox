726a0c891324b2c95d75e0b80f195f643199fb35
andela-cofor <oforchinedukelechi@gmail.com>
2018-03-22 17:06:30 +0100

    add user persistence methods

--- app/src/main/java/com/blackboxteam/app/blackbox/util/UserSessionPersistence.java
+++ app/src/main/java/com/blackboxteam/app/blackbox/util/UserSessionPersistence.java
@@ -1,20 +1,39 @@
 package com.blackboxteam.app.blackbox.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
 /**
  * UserSessionPersistence
  *
  * Manages user session
  */
 public class UserSessionPersistence {
+    private Context mContext;
+    private String user;
 
-    private boolean isActiveSession = false;
+    public UserSessionPersistence () {
 
-
-    public boolean isActiveSession() {
-        return isActiveSession;
     }
 
-    public void setActiveSession(boolean activeSession) {
-        isActiveSession = activeSession;
+    // Save the user login/signIp info
+    public void saveUserInfo(String email, String password) {
+        SharedPreferences sharedPreferences;
+        sharedPreferences = mContext.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", email);
+        editor.putString("password", password);
+        editor.apply();
     }
+
+    // get user information
+    public boolean getUserInfo(String sharedPrefName) {
+        SharedPreferences sharedPreferences;
+        sharedPreferences = mContext.getSharedPreferences(sharedPrefName, Context.MODE_PRIVATE);
+
+        return sharedPreferences == null;
+    }
+
 }
