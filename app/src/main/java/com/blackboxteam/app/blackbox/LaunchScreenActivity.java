b3d37e1a34c8a33756854756f1883c4de6a7f23b
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2018-05-10 10:48:18 +0100

    show login_signup button If there is no active user session

--- app/src/main/java/com/blackboxteam/app/blackbox/LaunchScreenActivity.java
+++ app/src/main/java/com/blackboxteam/app/blackbox/LaunchScreenActivity.java
@@ -1,20 +1,42 @@
 package com.blackboxteam.app.blackbox;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.blackboxteam.app.blackbox.util.UserSessionPersistence;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 public class LaunchScreenActivity extends AppCompatActivity {
 
+    UserSessionPersistence mUserSessionPersistence;
+    @BindView(R.id.launchScreen)
+    LinearLayout launchScreen;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
         ButterKnife.bind(this);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mUserSessionPersistence = new UserSessionPersistence(sharedPreferences);
+//        mUserSessionPersistence.setActiveUserSession(true);
+
+        if(mUserSessionPersistence.isActiveUserSession()) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        } else {
+            launchScreen.setVisibility(View.VISIBLE);
+        }
     }
 
     @OnClick(R.id.launchScreenLoginButton)
